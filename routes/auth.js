ef7c3ac68ba786eb613245752c16a5a200b211ce
Angel Travieso <angeltraviesoc@gmail.com>
2022-05-30 09:11:37 -0400

    formulario registro

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { 
+    loginForm,
+    registerUser,
+     registerForm,
+ } = require('../controllers/authController');
 
-router.get('/login', (req, res) => {
-    res.render('login');
-});
+
+router.get('/register', registerForm);
+router.post('/register', registerUser);
+router.get('/login', loginForm);
+
+
 
 module.exports = router;
